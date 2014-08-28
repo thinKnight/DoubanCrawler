0b62620e79cb142c5e30e6850f03ce5e4d2f496e
thinKnight <jeeyeyeah@gmail.com>
2014-08-28 23:56:31 +0800

    Update doubanCrawler.py

--- doubanCrawler.py
+++ doubanCrawler.py
@@ -1,36 +1,38 @@
 # -*- coding: UTF-8 -*-
+# 如果要在python2的py文件里面写中文，则必须要添加一行声明文件编码的注释，否则python2会默认使用ASCII编码。  
+
 import re 
 import urllib2
 
 def douban_crawler(url_head, target):
     for page in range(0, 1000, 20):
-
+    #这个1000是检索的条目数量，可以按需设定
         url_rear = "?start=%d&type=T" % page
         url_use = url_head + url_rear
-
+        #两段合成真正的url
         content = urllib2.urlopen(url_use).read()
         content = content.decode("UTF-8").encode("UTF-8")
-
+        
         content = content.replace(r'title="去FM收听"', "")
         content = content.replace(r'title="去其他标签"', "")
-
+        
         name = re.findall(r'title="(\S*?)"', content, re.S)
-       
+        #正则表达式捕获标题
         num  = re.findall(r'<span\s*class="rating_nums">([0-9.]*)<\/span>', content)
-
+        #正则表达式捕获分数
         
         doc = zip(name, num)
-        
+        #将标题和分数打包成([ , ][ , ]...)的形式
         if target == "book":
-            dou = open("doc_book", 'a')
+            dou = open("doc_book.txt", 'a')
         elif target == "music":
-            dou = open("doc_music", 'a')
+            dou = open("doc_music.txt", 'a')
         elif target == "movie":
-            dou = open("doc_movie", 'a')
+            dou = open("doc_movie.txt", 'a')
 
         for i in doc:
             dou.write(i[0] + " " + i[1] + "\n")
-            
+            #写入
     dou.close()
 
 if __name__ == '__main__':
@@ -41,8 +43,8 @@ if __name__ == '__main__':
     url_head  = "http://%s.douban.com/tag/%s" % (target, tag)
 
     douban_crawler(url_head, target)
-
-
+    
+    print "抓取完毕"
 
 
 
